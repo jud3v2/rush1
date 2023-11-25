62830d68f76ffa88d2306ccaa819f1131b415c2d
jud3v2 <judikaelbellance@icloud.com>
2023-11-25 16:03:37 +0100

    ajout des fichier de l'étape 3

---
+++ rush1_step3/my_script.js
@@ -0,0 +1,7 @@
+console.log("Bonjour je suis connecté");
+
+// Faire la validation du formulaire en javascript
+
+// Afficher les champs en erreur en rouge. et afficher un message en rouge
+// en bas du formulaire pour afficher les erreur de manière précise.
+
